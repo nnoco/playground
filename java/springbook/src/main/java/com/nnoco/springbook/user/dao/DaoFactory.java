8bf63572718a9525c597157e32a88cdc6e75939f
Junyoung Lee <junyoung.plum@gmail.com>
2016-07-21 03:05:54 +0900

    [springbook] 부가기늠 추가 - CountingConnectionMaker

--- java/springbook/src/main/java/com/nnoco/springbook/user/dao/DaoFactory.java
+++ java/springbook/src/main/java/com/nnoco/springbook/user/dao/DaoFactory.java
@@ -1,18 +1,30 @@
 package com.nnoco.springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+	@Bean
 	public UserDao userDao() {
 //		ConnectionMaker connectionMaker = new KConnectionMaker();
 		return new UserDao(connectionMaker());
 	}
 	
+	@Bean
 	public AccountDao accountDao() {
 		// KConnectionMaker 를 생성하는 코드 중복
 //		ConnectionMaker connectionMaker = new KConnectionMaker();
 		return new AccountDao(connectionMaker());
 	}
 	
+	@Bean
 	public ConnectionMaker connectionMaker() {
+		return new CountingConnectionMaker(realConnectionMaker());
+	}
+	
+	@Bean
+	public ConnectionMaker realConnectionMaker() {
 		return new KConnectionMaker();
 	}
 }
--- java/springbook/src/main/java/com/nnoco/springbook/user/dao/UserDaoTest.java
+++ java/springbook/src/main/java/com/nnoco/springbook/user/dao/UserDaoTest.java
@@ -2,24 +2,36 @@ package com.nnoco.springbook.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.nnoco.springbook.user.domain.User;
 
 public class UserDaoTest {
 	public static void main( String[] args ) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+		// Spring 적용 - Application Context 활용
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
+		
+		daoTest(dao, "nnoco9", "이준영", "Hello");
+		
+		CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+		System.out.println(ccm.getCounter());
+    }
+	
+	static void daoTest(UserDao userDao, String id, String name, String password) throws ClassNotFoundException, SQLException {
+		User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setPassword(password);
         
-        User user = new User();
-        user.setId("nnoco6");
-        user.setName("이준영");
-        user.setPassword("hello");
-        
-        dao.add(user);
+        userDao.add(user);
         
         System.out.println(user.getId() + " 등록 성공");
         
-        User user2 = dao.get("nnoco4");
+        User user2 = userDao.get(id);
         System.out.println(user2.getName());
         System.out.println(user2.getPassword());
         System.out.println(user2.getId() + " 조회 성공");
-    }
+	}
 }
